862687ea3b29f0909d7b2d460166d98524fdc220
jessikasinghh <128406656+jessikasinghh@users.noreply.github.com>
2025-01-17 14:46:02 +0530

    addition of 2 numbers

---
+++ src/addition.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class addition {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the no:");
+        Double num = sc.nextDouble();
+        System.out.println("enter the no:");
+        Double num2 = sc.nextDouble();
+        System.out.println(num + num2);
+    }
+}
